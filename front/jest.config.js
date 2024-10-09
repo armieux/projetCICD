c39c158727afa816d988ad4f313a2860f6c37a76
BDanielo <daniel@armieux.fr>
2024-10-09 16:33:05 +0200

    Fix(test) : use junit format

--- front/jest.config.js
+++ front/jest.config.js
@@ -4,4 +4,11 @@ module.exports = {
         '^.+\\.jsx?$': 'babel-jest', // Transform JSX with babel-jest
     },
     setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Run this setup file after Jest has been configured
+    reporters: [
+        'default',
+        ['jest-junit', {
+            outputDirectory: './reports', // Specify the output directory
+            outputName: 'junit.xml', // Specify the output file name
+        }]
+    ],
 };
